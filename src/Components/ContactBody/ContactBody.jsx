3f585a760c5a51c6f48d0dd8c5c481a67a26ba74
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2025-05-08 12:54:21 +0600

    added some functionality on buttons and used useState -- done

--- src/Components/ContactBody/ContactBody.jsx
+++ src/Components/ContactBody/ContactBody.jsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ContactBody.module.css";
 import { MdMessage } from "react-icons/md";
 import { IoIosCall } from "react-icons/io";
 import { MdEmail } from "react-icons/md";
 
-
 const ContactBody = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [text, setText] = useState();
+  // console.log("ok")
+  const onViaCall = () => {
+    console.log("I'm from call");
+  };
+  const onViaSupportChat = () => {
+    console.log("I'm from support chat");
+  };
+  const onViaEmailForm = () => {
+    console.log("I'm from email form");
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setName (event.target[0].value);
+    setEmail (event.target[1].value);
+    setText (event.target[2].value);
+  };
   return (
     <div className={`${styles.contactBody}`}>
       <div className={`${styles.contactBodyForm}`}>
         <div className={styles.contactBtns}>
-          <button>
+          <button onClick={onViaSupportChat}>
             <MdMessage /> Via Support Chat
           </button>
-          <button>
+          <button onClick={onViaCall}>
             <IoIosCall /> Via Call
           </button>
         </div>
         <div className={styles.viaEmailBtn}>
-          <button>
+          <button onClick={onViaEmailForm}>
             <MdEmail /> Via Email Form
           </button>
         </div>
-        <div className={styles.formInputs}>
+        <form onSubmit={onSubmit} className={styles.formInputs}>
           <div>
             <label htmlFor="name">Name</label>
             <input type="Name" placeholder="" name="name" />
@@ -35,10 +53,11 @@ const ContactBody = () => {
             <label htmlFor="text">Text</label>
             <textarea rows={5} type="Text" name="text" />
           </div>
-        </div>
-        <div className={`${styles.contactBtns} ${styles.submitBtn}`}>
-          <button>Submit</button>
-        </div>
+          <div className={`${styles.contactBtns} ${styles.submitBtn}`}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        <label htmlFor=""> Name: {name} <br/>Email:  {email} <br />Text: {text}</label>
       </div>
       <div className={styles.contactBodyImg}>
         <img src="../../../public/images/Service 24_7-pana 1.svg" alt="" />
